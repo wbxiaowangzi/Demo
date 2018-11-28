19664926e800e111d9e27874d39e09ad29dcd612
YingZi <wangbo@myreal3d.com>
2018-11-28 13:00:47 +0800

    add python

---
+++ Demo/C++/FirstBlood.cpp
@@ -0,0 +1,16 @@
+//
+//  FirstBlood.cpp
+//  Demo
+//
+//  Created by YingZi on 2018/11/15.
+//  Copyright © 2018 wangbo. All rights reserved.
+//
+
+#include "FirstBlood.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello,world" << std::endl;
+    return 0;
+}
---
+++ Demo/C++/FirstBlood.hpp
@@ -0,0 +1,14 @@
+//
+//  FirstBlood.hpp
+//  Demo
+//
+//  Created by YingZi on 2018/11/15.
+//  Copyright © 2018 wangbo. All rights reserved.
+//
+
+#ifndef FirstBlood_hpp
+#define FirstBlood_hpp
+
+#include <stdio.h>
+
+#endif /* FirstBlood_hpp */
