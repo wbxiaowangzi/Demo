cf69fa1f37323c838cfb6aceb389e7e8718d0dd1
WangBo <wangbo@myreal3d.com>
2019-08-22 11:14:56 +0800

    update

---
+++ Demo/Items/JSPatch/OCFiles/JSPatchTestVC.h
@@ -0,0 +1,17 @@
+//
+//  JSPatchTestVC.h
+//  Demo
+//
+//  Created by WangBo on 2019/8/22.
+//  Copyright © 2019 wangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JSPatchTestVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo/Items/JSPatch/OCFiles/JSPatchTestVC.m
@@ -0,0 +1,37 @@
+//
+//  JSPatchTestVC.m
+//  Demo
+//
+//  Created by WangBo on 2019/8/22.
+//  Copyright © 2019 wangbo. All rights reserved.
+//
+
+#import "JSPatchTestVC.h"
+#import "JPEngine.h"
+#import <Foundation/Foundation.h>
+
+@interface JSPatchTestVC ()
+
+@end
+
+@implementation JSPatchTestVC
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self loadAndEvaluteSript];
+}
+
+- (void)loadAndEvaluteSript{
+    NSString *urlStr = @"https://pan.baidu.com/s/1Qw82oZCDCAQkwwjCJWitXw";
+    NSURL *url = [NSURL URLWithString:urlStr];
+    
+    [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSString *script = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+        [JPEngine evaluateScript:script];
+    }];
+}
+
+
+
+@end
