c4343319efafcc4204d1ca4055b03ae99882f35b
WangBo <wangbo@myreal3d.com>
2019-08-22 11:43:44 +0800

    update

--- Demo/Items/JSPatch/OCFiles/JSPatchTestVC.m
+++ Demo/Items/JSPatch/OCFiles/JSPatchTestVC.m
@@ -23,12 +23,15 @@
 }
 
 - (void)loadAndEvaluteSript{
-    NSString *urlStr = @"https://github.com/wbxiaowangzi/Demo/blob/master/Demo/Items/JSPatch/ScriptFiles/jsscript.txt";
-    NSURL *url = [NSURL URLWithString:urlStr];
     
-    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSString *script = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
-        [JPEngine evaluateScript:script];
+    NSString *urlStr = @"https://raw.githubusercontent.com/wbxiaowangzi/Demo/master/Demo/Items/JSPatch/ScriptFiles/jsscript.txt";
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSURLSessionDownloadTask *task = [[NSURLSession sharedSession] downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (location != nil){
+            NSData *data = [NSData dataWithContentsOfFile:location.path];
+            NSString *script = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+            [JPEngine evaluateScript:script];
+        }
     }];
     [task resume];
 }
---
+++ Demo/Items/JSPatch/ScriptFiles/jsscript.js
@@ -0,0 +1,6 @@
+\
+var alertView = require('UIAlertView').alloc().init();\
+alertView.setTitle('Alert');\
+alertView.setMessage('AlertView from js'); \
+alertView.addButtonWithTitle('OK');\
+alertView.show(); \
