229321d90175bea337ecdcd539cb33a473656ff9
wangbo <wangbomvp@163.com>
2023-03-13 10:25:44 +0800

    update

--- Demo/Instrument/Util/MemoryHelper.swift
+++ Demo/Instrument/Util/MemoryHelper.swift
@@ -15,33 +15,36 @@ class MemoryHelper {
      请注意，这些方法使用了底层的系统调用来获取内存信息。因此，这些方法可能不适用于所有iOS版本和设备。此外，使用这些方法可能会对性能产生一定的影响，因此请谨慎使用。
      */
     
-    static func usedMemory() -> UInt64 {
-        var taskInfo = mach_task_basic_info()
-        var count = mach_msg_type_number_t(MACH_TASK_BASIC_INFO_COUNT)
-        let kerr = withUnsafeMutablePointer(to: &taskInfo) {
-            taskInfoPtr in
-            taskInfoPtr.withMemoryRebound(to: integer_t.self, capacity: 1) {
-                taskInfoIntPtr in
-                task_info(mach_task_self_,
-                          task_flavor_t(MACH_TASK_BASIC_INFO),
-                          taskInfoIntPtr,
-                          &count)
+    func usedMemory() -> UInt64 {
+        var info = task_basic_info()
+        var count = mach_msg_type_number_t(MemoryLayout<task_basic_info>.size) / 4
+        let kerr: kern_return_t = withUnsafeMutablePointer(to: &info) {
+            $0.withMemoryRebound(to: integer_t.self, capacity: 4) {
+                task_info(mach_task_self_, task_flavor_t(TASK_BASIC_INFO), $0, &count)
             }
         }
-        guard kerr == KERN_SUCCESS else { return 0 }
-        return taskInfo.resident_size
+
+        guard kerr == KERN_SUCCESS else {
+            return 0
+        }
+
+        return UInt64(info.resident_size)
     }
-    
-    static func freeMemory() -> UInt64 {
-        var stats = vm_statistics_data_t()
+
+    func freeMemory() -> UInt64 {
+        var vmStat = vm_statistics_data_t()
         var count = UInt32(MemoryLayout<vm_statistics_data_t>.size / MemoryLayout<integer_t>.size)
-        let result = withUnsafeMutablePointer(to: &stats) {
+        let kerr: kern_return_t = withUnsafeMutablePointer(to: &vmStat) {
             $0.withMemoryRebound(to: integer_t.self, capacity: Int(count)) {
                 host_statistics(mach_host_self(), HOST_VM_INFO, $0, &count)
             }
         }
-        guard result == KERN_SUCCESS else { return 0 }
-        let freeCount = UInt64(stats.free_count) * UInt64(vm_page_size)
-        return freeCount
+
+        guard kerr == KERN_SUCCESS else {
+            return 0
+        }
+
+        return UInt64(vmStat.free_count) * UInt64(vm_page_size)
     }
+
 }
